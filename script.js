c0961ca2726a7d2910bef694ced25af76da21cc2
PloKoon68 <ors_mehmet68@hotmail.com>
2025-11-09 15:22:06 +0300

    element drawing tested on canvas

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById('seasawCnvs');  //main canvas element
+const cheight = canvas.height
+const cwidth = canvas.width
+
 const ctx = canvas.getContext('2d');   //for drawing objects in canvas
 
 
+ctx.fillStyle = 'red';
+ctx.fillRect(cwidth * 0.6, cheight * 0.1, 50, 50 * cheight / cwidth);
+ctx.fillStyle = 'black';
+ctx.font = '20px Arial';
+ctx.fillText('Canvas Working!', 200, 100);
