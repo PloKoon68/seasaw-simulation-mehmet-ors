ec99f3e740122855f45a1b9beb149e662726cebd
PloKoon68 <ors_mehmet68@hotmail.com>
2025-11-09 18:53:59 +0300

    seasaw drawed

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const canvas = document.getElementById('seasawCnvs');  //main canvas element
-const cheight = canvas.height
-const cwidth = canvas.width
+
+//note: this system is assumed to have a square shape canvas (width=height)
+const LENGTH = canvas.height
 
 const ctx = canvas.getContext('2d');   //for drawing objects in canvas
 
 
+
 //p prefix means percentage, instead of raw pixels
-const pHeight = (percentage) => {
-    return percentage * cheight / 100;
+const percentage_to_px = (percentage) => {
+    return percentage * LENGTH / 100;
 }
-const pWidth = (percentage) => {
-    return percentage * cwidth / 100;
-}
+
 
 const pfillRectWith = (x, w, y, h, color) => {
     ctx.fillStyle = color;
-    ctx.fillRect(pWidth(x), pHeight(y), pWidth(w), pHeight(h));
+    ctx.fillRect(percentage_to_px(x), percentage_to_px(y), percentage_to_px(w), percentage_to_px(h));
 }
 
 
 const pdrawShape = (coordinates, color) => {
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.moveTo(pWidth(coordinates[0][0]), pHeight(coordinates[0][1])); 
+    ctx.moveTo(percentage_to_px(coordinates[0][0]), percentage_to_px(coordinates[0][1])); 
     for(let i = 1; i < coordinates.length; i++) {
-        ctx.lineTo(pWidth(coordinates[i][0]), pHeight(coordinates[i][1]));
+        ctx.lineTo(percentage_to_px(coordinates[i][0]), percentage_to_px(coordinates[i][1]));
     }
     ctx.closePath();
     ctx.fill();  
@@ -36,11 +36,36 @@ pfillRectWith(0, 100, 80, 20, '#0d8a41ff')
 //draw pivot triangle
 pdrawShape([[50, 70], [45, 80], [55, 80]], '#5d6767ff');
 
+//draw seasaw
+pdrawShape([[10, 74], [90, 74], [90, 69], [10, 69]], '#8f5509ff');
 
-const PLANK_LENGTH = 400; 
-const PLANK_HEIGHT = 20; 
-const CENTER_X = canvas.width / 2; 
-const CENTER_Y = canvas.height / 2 + 50;
+
+function drawBall(x, y, radius, color, weight) {
+    // Shadow
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 1);
+    ctx.fill();
+    
+    // Ball
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+    
+    // Outline
+    ctx.strokeStyle = '#2c3e50';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    
+    // Weight label
+    ctx.fillStyle = 'white';
+    ctx.font = 'bold 12px Arial';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(weight + 'kg', x, y);
+}
+
 
 
 /*
