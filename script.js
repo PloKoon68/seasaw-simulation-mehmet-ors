2469db0b71c0aa23c3237ea4a582a31e0cf423b8
PloKoon68 <ors_mehmet68@hotmail.com>
2025-11-09 17:11:40 +0300

    ground added

--- script.js
+++ script.js
@@ -4,9 +4,29 @@ const cwidth = canvas.width
 
 const ctx = canvas.getContext('2d');   //for drawing objects in canvas
 
+const pHeight = (percentage) => {
+    return percentage * cheight / 100;
+}
+const pWidth = (percentage) => {
+    return percentage * cwidth / 100;
+}
 
-ctx.fillStyle = 'red';
-ctx.fillRect(cwidth * 0.6, cheight * 0.1, 50, 50 * cheight / cwidth);
+const pfillRectWith = (x, w, y, h) => {
+    ctx.fillRect(pWidth(x), pHeight(y), pWidth(w), pHeight(h));
+}
+
+// Draw ground
+ctx.fillStyle = '#0d8a41ff';
+pfillRectWith(0, 100, 80, 20)
+
+
+
+/*
 ctx.fillStyle = 'black';
 ctx.font = '20px Arial';
-ctx.fillText('Canvas Working!', 200, 100);
+ctx.fillText('Test text!', 200, 100);
+*/
+
+// Clear canvas (wipe everything)
+//ctx.clearRect(50, 50, canvas.width, canvas.height);
+
